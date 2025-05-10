28636d29e8a631dca473d8aa4a270c02e0a6f4d1
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-05-10 17:32:35 -0300

    feat: add tailwindcss with nativewind

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import RootNavigator from './src/navigation';
 import { PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 
+import "./src/styles/global.css"
+
 export default function App() {
   return (
     <PaperProvider>
--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    env: {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+     env: {
       production: {
         plugins: ['react-native-paper/babel'],
       },

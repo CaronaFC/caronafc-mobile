da171c142fa508b94f314bc0e45d077293d6b006
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2025-05-18 20:49:43 -0300

    feat: add default textinput

---
+++ src/components/commom/TextInput.tsx
@@ -0,0 +1,40 @@
+import { Icon, Input } from "@ui-kitten/components";
+import React, { useState } from "react";
+import { Feather } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+
+type Props = {
+  value: string;
+  setValue: (text: string) => void;
+  placeholder?: string;
+  type?: "text" | "password";
+};
+
+export default function TextInput({
+  value,
+  setValue,
+  placeholder = "",
+  type = "text",
+}: Props) {
+  const isPassword = type === "password";
+  const [secure, setSecure] = useState(isPassword);
+  const toggleSecureEntry = () => setSecure(!secure);
+  const renderIcon = () => (
+    <TouchableOpacity onPress={toggleSecureEntry}>
+      <Feather name={secure ? "eye-off" : "eye"} size={20} color="#8F9BB3" />
+    </TouchableOpacity>
+  );
+  return (
+    <Input
+      placeholder={placeholder}
+      value={value}
+      onChangeText={setValue}
+      className="text-input"
+      style={{
+        backgroundColor: "#F2F3F3",
+      }}
+      secureTextEntry={isPassword ? secure : false}
+      accessoryRight={isPassword ? renderIcon : undefined}
+    />
+  );
+}

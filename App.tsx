e5e90698358de7e89eeeede63512f8d9e82dc8e1
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-05-19 20:26:55 -0300

    feat: safeareaprovider

--- App.tsx
+++ App.tsx
@@ -2,19 +2,20 @@ import "react-native-gesture-handler";
 import "./src/styles/global.css";
 
 import React from "react";
-
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationContainer } from "@react-navigation/native";
 import TabNavigator from "./src/navigation/TabNavigator";
 import { ApplicationProvider } from "@ui-kitten/components";
 import * as eva from "@eva-design/eva";
+
 export default function App() {
   return (
-    <>
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <NavigationContainer>
-          <TabNavigator />
-        </NavigationContainer>
-      </ApplicationProvider>
-    </>
+      <SafeAreaView style={{ flex: 1 }} edges={['top', 'bottom']}>
+        <ApplicationProvider {...eva} theme={eva.light}>
+          <NavigationContainer>
+            <TabNavigator />
+          </NavigationContainer>
+        </ApplicationProvider>
+      </SafeAreaView>
   );
 }

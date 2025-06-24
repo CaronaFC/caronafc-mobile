d82ab5504123140afe27a7c1ebf7a6418104acb4
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2025-06-24 20:31:04 -0300

    fix: change to backend private ip

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.API_URL || "http://localhost:3000";
+const API_URL = "http://192.168.1.13:3000";
 
 export const api = axios.create({
     baseURL: API_URL,

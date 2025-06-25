5ba5d7d1f7b065baefd1e9c3c18c8955bb5cc88f
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2025-06-25 06:11:00 -0300

    feat: add auth services api calls

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { api } from "../lib/api"
-import { RegisterResponseType, RegisterUserType } from "../types/auth";
+import { LoginResponseType, LoginUserType, RegisterResponseType, RegisterUserType } from "../types/auth";
 
 
 export async function registerUser(userData: RegisterUserType): Promise<RegisterResponseType> {
@@ -11,6 +11,26 @@ export async function registerUser(userData: RegisterUserType): Promise<Register
     } catch (error) {
         if (axios.isAxiosError(error)) {
             const status = error.response?.status
+            const message = error.response?.data?.message || "Erro na requisição"
+            throw new Error(`Erro ${status ?? "desconhecido"}: ${message}`);
+        } else {
+            throw new Error("Erro inesperado ao registrar usuário.");
+        }
+    }
+}
+
+
+export async function loginUser(userData: LoginUserType): Promise<AxiosResponse<LoginResponseType>> {
+    try {
+        const response = await api.post("/auth/login", userData)
+        return response
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            const status = error.response?.status
+            if (status == 401) {
+                console.log("Credenciais inválidas")
+                throw new Error(`Credenciais inválidas`);
+            }
             const message = error.response?.data?.message || "Erro na requisição"
             throw new Error(`Erro ${status ?? "desconhecido"}: ${message}`);
         } else {

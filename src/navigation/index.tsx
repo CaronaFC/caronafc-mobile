f7fe1422e4c7ed6d1ef8f2676387afff4944534b
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2025-06-25 06:09:11 -0300

    feat: protect navigation stack with auth

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -6,6 +6,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import RegisterScreen from "../screens/RegisterScreen";
 import TabNavigator from "./TabNavigator";
 import VehicleCreationScreen from "../screens/VehicleCreationScreen";
+import { useAuth } from "../context/AuthContext";
+import { ActivityIndicator, View } from "react-native";
 
 export type RootStackParamList = {
   Login: undefined;
@@ -20,29 +22,33 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootNavigator() {
-  return (
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen
-        name="Login"
-        component={LoginScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen name="Register" component={RegisterScreen} />
-      <Stack.Screen
-        name="Home"
-        component={TabNavigator}
-        options={{ headerShown: false }}
-      />
 
-      <Stack.Screen
-        name="VehicleCreation"
-        component={VehicleCreationScreen}
-        options={{
-          title: "Cadastro de Veículo",
-        }}
-      />
+  const { isLoading, userToken } = useAuth()
+  if (isLoading) {
+    return (
+
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size={'large'} />
+      </View>
+    )
+  }
+  return (
+    <Stack.Navigator>
+      {userToken == null ? (
+        <>
+          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Home" component={TabNavigator} options={{ headerShown: false }} />
+          <Stack.Screen
+            name="VehicleCreation"
+            component={VehicleCreationScreen}
+            options={{ title: "Cadastro de Veículo", headerShown: true }}
+          />
+        </>
+      )}
     </Stack.Navigator>
   );
 }

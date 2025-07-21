a87ccf3ff84cd4b7696f60838606e27305f13288
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-07-21 12:08:46 -0300

    refactored request screen

--- src/screens/MyTravelRequestsScreen.tsx
+++ src/screens/MyTravelRequestsScreen.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigation";
+import { FontAwesome5 } from "@expo/vector-icons";
 
-import { SolicitationItem } from "../components/requests/SolicitationItem";
+import { RequestItem } from "../components/requests/RequestItem";
 import { fetchSolicitationPassenger } from "../services/requestsService";
-import { Solicitacao } from "../types/request";
+import { Request } from "../types/request";
 
 type ProfileScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -16,47 +17,46 @@ type ProfileScreenNavigationProp = NativeStackNavigationProp<
 export default function MyTravelRequestsScreen() {
   const navigation = useNavigation<ProfileScreenNavigationProp>();
 
-  const [solicitacoes, setSolicitacoes] = useState<Solicitacao[]>([]);
-  const [loading, setLoading] = useState(false);
+  const [solicitacoes, setSolicitacoes] = useState<Request[]>([]);
 
-  const fetchSolicitacoes = async () => {
+  const fetchRequests = async () => {
     try {
       const res = await fetchSolicitationPassenger();
       setSolicitacoes(res);
-      console.log(res);
     } catch (error) {
       console.error(error);
-    } finally {
-      setLoading(false);
-    }
+    } 
   };
 
   useEffect(() => {
-    setLoading(true);
-    fetchSolicitacoes();
+    fetchRequests();
   }, []);
 
-  if (loading) {
-    return (
-      <View className="p-4">
-        <Text className="text-center text-xl">Carregando solicitações...</Text>
-      </View>
-    );
-  }
-
   return (
-    <FlatList
-      className="p-4"
-      data={solicitacoes}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => <SolicitationItem item={item} />}
-      ListEmptyComponent={
-        <View>
-          <Text className="text-center text-xl">
-            Nenhuma solicitação encontrada.
-          </Text>
-        </View>
-      }
-    />
+    <View className="flex-1 p-5 bg-gray-50">
+       <View className="mb-5">
+        <TouchableOpacity
+          onPress={fetchRequests}
+          className="flex-row items-center bg-black px-5 py-3 rounded-md shadow"
+          activeOpacity={0.8}
+        >
+          <FontAwesome5 name="sync" size={18} color="#fff" />
+          <Text className="text-white ml-3 font-semibold text-lg">Atualizar</Text>
+        </TouchableOpacity>
+      </View>
+      {solicitacoes.length === 0 ? (
+        <Text className="text-center text-gray-500 text-lg mt-10">
+          Você ainda não possui solicitações.
+        </Text>
+      ) : (
+        <FlatList
+          data={solicitacoes}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={RequestItem}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      )}
+    </View>
   );
 }
